bf1d4c31708aa08f7dfceb403eb56a31aa5d6fc2
João Vitral <joaoviniciusvitral@hotmail.com>
2024-07-05 21:41:57 -0300

    new transition

--- sections/hero-section/components/background-component/Background.component.tsx
+++ sections/hero-section/components/background-component/Background.component.tsx
@@ -1,10 +1,10 @@
-import { useApp } from "@/context/AppContext";
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { background_data } from "@/data/background-data";
-import gsap from "gsap";
-import BGImage from "@/components/bgimage-component/BGImage.component";
-import { getNextCollection } from "@/utils/getNextCollection";
-import Image from "next/image";
+import { useApp } from "@/context/AppContext"
+import React, { useCallback, useEffect, useRef, useState } from "react"
+import { background_data } from "@/data/background-data"
+import gsap from "gsap"
+import BGImage from "@/components/bgimage-component/BGImage.component"
+import { getNextCollection } from "@/utils/getNextCollection"
+import Image from "next/image"
 
 const Background: React.FC = () => {
   const {
@@ -14,9 +14,9 @@ const Background: React.FC = () => {
       index: itemIndex, // Variável para definir o índice do item dentro da coleção
     },
     setActiveBackground,
-  } = useApp();
+  } = useApp()
 
-  const imageContainerRef = useRef<HTMLDivElement>(null);
+  const imageContainerRef = useRef<HTMLDivElement>(null)
 
   const fadeAnimation = useCallback(() => {
     if (imageContainerRef.current) {
@@ -24,35 +24,53 @@ const Background: React.FC = () => {
         imageContainerRef.current,
         { opacity: 1 },
         { opacity: 0, duration: 1, ease: "power1.inOut" }
-      );
+      )
       gsap.fromTo(
         imageContainerRef.current,
         { opacity: 0 },
         { opacity: 1, duration: 1, ease: "power1.inOut" }
-      );
+      )
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
     if (imageContainerRef.current) {
-      fadeAnimation();
+      fadeAnimation()
     }
-  }, [collection, itemIndex]);
+  }, [collection, itemIndex])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setActiveBackground({
-        collection: isDesktop ? getNextCollection(collection) : collection,
-              index: isDesktop || itemIndex === 1 ? 0 : 1,
-      });
-    }, 5000);
+      if (!isDesktop) {
+        // check if index is 1
+        if (itemIndex === 1) {
+          setActiveBackground({
+            collection: getNextCollection(collection),
+            index: 0,
+          })
+        } else {
+          setActiveBackground({
+            collection,
+            index: 1,
+          })
+        }
+      } else {
+        setActiveBackground({
+          collection: getNextCollection(collection),
+          index: 0,
+        })
+      }
+    }, 5000)
 
-    return () => clearInterval(interval);
-  }, [collection, itemIndex, setActiveBackground, isDesktop]);
+    return () => clearInterval(interval)
+  }, [collection, itemIndex, setActiveBackground, isDesktop])
 
   return (
     <div className="absolute inset-0 w-full h-[100vh] z-[-1]">
-      <div ref={imageContainerRef} className="relative w-full h-full grid grid-cols-1 md:grid-cols-2">
+      <div
+        ref={imageContainerRef}
+        className="relative w-full h-full grid grid-cols-1 md:grid-cols-2"
+      >
         <div className={`relative w-full h-full`}>
           <Image
             key={`${collection}-${itemIndex}`}
@@ -67,7 +85,9 @@ const Background: React.FC = () => {
         {isDesktop && (
           <div className={`relative w-full h-full`}>
             <Image
-              key={`${collection}-${(itemIndex + 1) % background_data[collection].length}`}
+              key={`${collection}-${
+                (itemIndex + 1) % background_data[collection].length
+              }`}
               src={background_data[collection][1].src}
               alt={background_data[collection][1].alt}
               layout="fill"
@@ -78,7 +98,7 @@ const Background: React.FC = () => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Background;
+export default Background
