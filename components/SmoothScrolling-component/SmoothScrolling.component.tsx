37e310516454af3bbb0ca147ad4bb62260af2ea5
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-07-15 23:31:35 -0300

    feat: adding smooth to nav items

--- components/SmoothScrolling-component/SmoothScrolling.component.tsx
+++ components/SmoothScrolling-component/SmoothScrolling.component.tsx
@@ -1,7 +1,43 @@
+// components/SmoothScrolling.tsx
 "use client";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import { useEffect } from "react";
+import { useLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }: { children: React.ReactNode }) {
+  const lenis = useLenis();
+  console.log("carreguei");
+
+  useEffect(() => {
+    const handleAnchorClick = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      console.log(target);
+
+      if (
+        target.tagName === "A" ||
+        target.tagName === "a" ||
+        target.tagName === "nav" ||
+        target.tagName === "li" ||
+        (target.tagName === "button" &&
+          target.getAttribute("href")?.startsWith("#"))
+      ) {
+        console.log("começa com #");
+        event.preventDefault();
+        const id = target.getAttribute("href")!.substring(1);
+        const element = document.getElementById(id);
+        if (element) {
+          lenis?.scrollTo(element);
+        }
+      }
+    };
+
+    document.addEventListener("click", handleAnchorClick);
+
+    return () => {
+      document.removeEventListener("click", handleAnchorClick);
+    };
+  }, [lenis]);
+
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
       {children}
