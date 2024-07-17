0b4cc207c9134a5441cb77ea5664c4196e8a6d80
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-07-17 16:34:20 -0300

    fix spacing

--- pages/collection/festas-viert/index.tsx
+++ pages/collection/festas-viert/index.tsx
@@ -34,7 +34,7 @@ function FestasViert({
           <title>Festas Viert</title>
           <meta name="description" content="Festas Viert" />
         </Head>
-        <div className="grid grid-cols-2 container mx-auto lg:grid-cols-3 gap-x-[2.14vw] gap-y-[4.17vh] lg:gap-y-[23.61vh]">
+        <div className="grid grid-cols-2 container mx-auto lg:grid-cols-3 gap-x-[2.14vw] gap-y-[4.17vh] lg:gap-y-[5vh]">
           {products.map((product: Product, index: number) => (
             <Link
               href={`/collection/festas-viert/product/${product.entryId}`}
