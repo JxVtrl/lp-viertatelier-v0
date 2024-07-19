f8e950dc75d29e9a94bd966ea4cdd7e53232f211
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-07-18 23:56:56 -0300

    chore: Update MouseBlobity component styles and dependencies

--- components/mouseblobity-component/MouseBlobity.component.tsx
+++ components/mouseblobity-component/MouseBlobity.component.tsx
@@ -4,39 +4,43 @@ import { useEffect } from "react";
 import useBlobity from "blobity/lib/react/useBlobity";
 import AOS from "aos";
 export const blobityConfig = {
-   licenseKey: "opensource",
-   focusableElementsOffsetX: 15,
-   focusableElementsOffsetY: 15,
-   dotColor: "#fff",
-   focusableElements:
-   "[data-blobity], li:not([data-no-blobity]), a:not([data-no-blobity]), button:not([data-no-blobity]), [data-blobity-tooltip]",
-   opacity: 0.2,
-   border: "1px solid #0e1016",
-   zIndex: 999,
-   size: 20,
-   invert: false,
-   magnetic: false, 
+  licenseKey: "opensource",
+  focusableElementsOffsetX: 15,
+  focusableElementsOffsetY: 15,
+  dotColor: "#fff",
+  color: "#fff",
+  fontColor: "#fff",
+  fontWeight: 500,
+  font: "'Montserrat', Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif",
+  focusableElements:
+    "[data-blobity], li:not([data-no-blobity]), a:not([data-no-blobity]), button:not([data-no-blobity]), [data-blobity-tooltip]",
+  opacity: 0.2,
+  border: "1px solid #0e1016",
+  zIndex: 999,
+  size: 20,
+  invert: false,
+  magnetic: false,
 };
 
 export default function MouseBlobity() {
-   const blobityInstance = useBlobity(blobityConfig);
-   //const { matches } = useMediaQuery("(max-width: 880px)");
+  const blobityInstance = useBlobity(blobityConfig);
+  //const { matches } = useMediaQuery("(max-width: 880px)");
 
-   useEffect(() => {
-      AOS.init();
-      if (blobityInstance.current) {
-         // eslint-disable-next-line
-         // @ts-ignore for debugging purposes or playing around
-         window.blobity = blobityInstance.current;
-      }
-   }, [blobityInstance]);
+  useEffect(() => {
+    AOS.init();
+    if (blobityInstance.current) {
+      // eslint-disable-next-line
+      // @ts-ignore for debugging purposes or playing around
+      window.blobity = blobityInstance.current;
+    }
+  }, [blobityInstance]);
 
-   useEffect(() => {
-      window.scrollTo({
-         top: 0,
-         left: 0,
-      });
-   }, []);
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, []);
 
-   return <div />;
+  return <div />;
 }
