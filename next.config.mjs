bb8663050b1d9457e9c7a254cee19abba510fd50
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-07-18 23:23:02 -0300

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "img.freepik.com", "images.ctfassets.net", "scontent-gig4-1.cdninstagram.com", "scontent-iad3-1.cdninstagram.com"],
+    domains: [
+      "images.unsplash.com",
+      "img.freepik.com",
+      "images.ctfassets.net",
+      "scontent-gig4-1.cdninstagram.com",
+      "scontent-iad3-1.cdninstagram.com",
+      "scontent-iad3-2.cdninstagram.com",
+    ],
   },
   env: {
-    INSTA_TOKEN: process.env.INSTA_TOKEN
-  }
+    INSTA_TOKEN: process.env.INSTA_TOKEN,
+  },
 };
 
 export default nextConfig;
