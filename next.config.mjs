ef2d10af2516e725b1e594f84bac6822f686ff7c
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-07-18 23:21:44 -0300

    fix next.cionfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "img.freepik.com", "images.ctfassets.net", "scontent-gig4-1.cdninstagram.com"],
+    domains: ["images.unsplash.com", "img.freepik.com", "images.ctfassets.net", "scontent-gig4-1.cdninstagram.com", "scontent-iad3-1.cdninstagram.com"],
   },
   env: {
     INSTA_TOKEN: process.env.INSTA_TOKEN
