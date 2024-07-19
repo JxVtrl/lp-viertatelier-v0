7abe4bb33530d89ebe4fad09ce29784ffac16c87
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-07-19 12:42:02 -0300

    sharp

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
+    remotePatterns: [
       "images.unsplash.com",
       "img.freepik.com",
       "images.ctfassets.net",
@@ -14,6 +14,16 @@ const nextConfig = {
   },
   env: {
     INSTA_TOKEN: process.env.INSTA_TOKEN,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.child_process = false;
+    }
+
+    return config;
   },
 };
 
