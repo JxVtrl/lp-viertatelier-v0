237b40238709a1cb0bc138873220f20e85fb6948
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-07-19 00:14:07 -0300

    fix

--- components/ProductCard-component/ProductCard.component.tsx
+++ components/ProductCard-component/ProductCard.component.tsx
@@ -15,7 +15,7 @@ export default function ProductCard({
 }) {
   return (
     <div
-      className={`${inter.className} ${styles.container} flex flex-col gap-5 hover:color-white`}
+      className={`${inter.className} ${styles.container} flex flex-col gap-5`}
     >
       <div
         className="relative w-full z-[999]"
